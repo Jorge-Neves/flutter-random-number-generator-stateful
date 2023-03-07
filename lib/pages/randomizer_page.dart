36746c82f4b6ad058d2ee1d173ca4a0f43ae74b6
Paulo Santos <paulo.jorge.ngs@gmail.com>
2023-03-07 10:17:08 +0000

    Add onPress function to Generate button widget

--- lib/pages/randomizer_page.dart
+++ lib/pages/randomizer_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class RandomizerPage extends StatefulWidget {
@@ -10,12 +12,33 @@ class RandomizerPage extends StatefulWidget {
 }
 
 class _RandomizerPageState extends State<RandomizerPage> {
+  int? _generatedNumber;
+
+  final randomGenerator = Random();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Random"),
+        title: const Text("Random Number"),
       ),
+      body: Center(
+        child: Text(
+          _generatedNumber?.toString() ??
+              "Click the button to generate your number.",
+          style: const TextStyle(fontSize: 17),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text("Generate"),
+        onPressed: () {
+          setState(() {
+            _generatedNumber = widget.min +
+                randomGenerator.nextInt(widget.max + 1 - widget.min);
+          });
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
